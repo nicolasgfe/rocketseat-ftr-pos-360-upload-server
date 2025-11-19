80cc450168128313975e0cc2f611e9d61d65f64d
Nicolas G. Fernandes <nicolasgfe3@gmail.com>
2025-11-19 18:51:22 -0300

    Adicionando Padrão do projeto e criando primeira rota da aplicação.

--- src/infra/http/server.ts
+++ src/infra/http/server.ts
@@ -1,13 +1,37 @@
-import { env } from '@/env'
 import { fastifyCors } from '@fastify/cors'
 import { fastify } from 'fastify'
+import {
+  hasZodFastifySchemaValidationErrors,
+  serializerCompiler,
+  validatorCompiler,
+} from 'fastify-type-provider-zod'
+import { uploadImageRoute } from './routes/upload-image'
 
 const server = fastify()
 
+server.setValidatorCompiler(validatorCompiler)
+server.setSerializerCompiler(serializerCompiler)
+
+server.setErrorHandler((error, request, reply) => {
+  if (hasZodFastifySchemaValidationErrors(error)) {
+    return reply.status(400).send({
+      message: 'Validate Error',
+      issues: error.validation,
+    })
+  }
+
+  // Envia o erro p/ alguma ferramenta de observabilidade (Sentry/Datadog/Grafana/OTel)
+  console.error(error)
+
+  return reply.status(500).send({
+    message: 'Internal Server Error.',
+  })
+})
+
 server.register(fastifyCors, { origin: '*' })
+
+server.register(uploadImageRoute)
 
 server.listen({ port: 3333, host: '0.0.0.0' }).then(() => {
   console.log('HTTP server running!')
 })
-
-
